2d68aae2935c01e9475e5a53dc3670da83fb7daa
David Vloedman <djvloedman@gmail.com>
2020-01-07 12:24:50 -0800

    Initial Commit

---
+++ classes/vehicle-class.js
@@ -0,0 +1,38 @@
+'use strict';
+
+class Vehicle {
+  constructor(name, wheels) {
+    this.name = name;
+    this.wheels = wheels;
+  }
+
+  drive() {
+    return 'Moving Forward';
+  }
+
+  stop() {
+    return 'Stopping';
+  }
+
+}
+
+class Car extends Vehicle {
+  constructor(name) {
+    super(name, 4);
+  }
+}
+
+class Motorcycle {
+  constructor(name) {
+    super(name, 2);
+  }
+
+  wheelie() {
+    return 'Wheee!';
+  }
+}
+
+module.exports = {
+  Car,
+  Motorcycle
+}
