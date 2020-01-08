ea55042d9a4f58bbc7fb801e92ceea5c548ee8f1
David Vloedman <djvloedman@gmail.com>
2020-01-08 09:07:17 -0800

    Got JSDocs to work!

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -1,32 +1,50 @@
 'use strict';
-
+/**
+ * Vehicle
+ * @class
+ */
 class Vehicle {
   constructor(name, wheels) {
     this.name = name;
     this.wheels = wheels;
   }
-
+  /**
+ * Drive the vehicle
+ * @returns {string}
+ */
   drive() {
     return 'Moving Forward';
   }
-
+  /**
+ * Stop the vehicle
+ * @returns {string}
+ */
   stop() {
     return 'Stopping';
   }
 
 }
-
+/**
+ * Car 
+ * @class
+ */
 class Car extends Vehicle {
   constructor(name) {
     super(name, 4);
   }
 }
-
+/**
+ * Motorcycle 
+ * @class
+ */
 class Motorcycle extends Vehicle {
   constructor(name) {
     super(name, 2);
   }
-
+  /**
+ * Do a wheelie!
+ * @returns {string}
+ */
   wheelie() {
     return 'Wheee!';
   }
